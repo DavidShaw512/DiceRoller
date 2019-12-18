2be00792b77b39a7557b707dcdfa50f271dec8e4
Admin <admin@AdminsMacBook7.attlocal.net>
2019-12-18 14:50:14 -0600

    App functional

---
+++ src/components/die.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import d20 from "../img/d20.png"
+import d12 from "../img/d12.png"
+import d10 from "../img/d10.png"
+import d8 from "../img/d8.png"
+import d6 from "../img/d6.png"
+import d4 from "../img/d4.png"
+import './die.css';
+
+const Die = (props) => {
+
+    const results = props.results.join(' / ');
+
+    const dieCount = props.results.length;
+
+    const total = props.results.reduce((a, b) => a + b, 0);
+
+    let image;
+    if (props.sides === 20) {
+        image = d20;
+    } else if (props.sides === 12) {
+        image = d12;
+    } else if (props.sides === 10) {
+        image = d10;
+    } else if (props.sides === 8) {
+        image = d8;
+    } else if (props.sides === 6) {
+        image = d6;
+    } else if (props.sides === 4) {
+        image = d4;
+    }
+
+    return (
+        <section className="die">
+            <button className="dieButton" onClick={props.click}>
+                <img className="dieImg" src={image} alt="die icon"/>
+                <p className="dieCounts">Dice: {dieCount}<br/>
+                Total: {total}
+                </p>
+            </button>
+            <div className="result">
+                <p>{results}</p>
+            </div>
+        </section>
+    )
+}
+
+
+export default Die;
--- src/components/rollTable.js
+++ src/components/rollTable.js
@@ -1,20 +1,70 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 
-import Dice from './dice';
+import Die from './die';
 import './rollTable.css';
 
 class RollTable extends React.Component {
-    
+    state = {
+        d20results: [],
+        d12results: [],
+        d10results: [],
+        d8results: [],
+        d6results: [],
+        d4results: []
+    }
+
+    roll20 = () => {
+        const result = Math.ceil(Math.random() * 20);
+        this.setState ({
+            d20results: [...this.state.d20results, result]
+        })
+    }
+
+    roll12 = () => {
+        const result = Math.ceil(Math.random() * 12);
+        this.setState ({
+            d12results: [...this.state.d12results, result]
+        })
+    }
+
+    roll10 = () => {
+        const result = Math.ceil(Math.random() * 10);
+        this.setState ({
+            d10results: [...this.state.d10results, result]
+        })
+    }
+
+    roll8 = () => {
+        const result = Math.ceil(Math.random() * 8);
+        this.setState ({
+            d8results: [...this.state.d8results, result]
+        })
+    }
+
+    roll6 = () => {
+        const result = Math.ceil(Math.random() * 6);
+        this.setState ({
+            d6results: [...this.state.d6results, result]
+        })
+    }
+
+    roll4 = () => {
+        const result = Math.ceil(Math.random() * 4);
+        this.setState ({
+            d4results: [...this.state.d4results, result]
+        })
+    }
+
     render() {
         return (
             <section className="rollTable">
-                <Dice sides={20}/>
-                <Dice sides={12}/>
-                <Dice sides={10}/>
-                <Dice sides={8}/>
-                <Dice sides={6}/>
-                <Dice sides={4}/>
+                <Die sides={20} click={() => this.roll20()} results={this.state.d20results}/>
+                <Die sides={12} click={() => this.roll12()} results={this.state.d12results}/>
+                <Die sides={10} click={() => this.roll10()} results={this.state.d10results}/>
+                <Die sides={8} click={() => this.roll8()} results={this.state.d8results}/>
+                <Die sides={6} click={() => this.roll6()} results={this.state.d6results}/>
+                <Die sides={4} click={() => this.roll4()} results={this.state.d4results}/>
             </section>
         )
     }
